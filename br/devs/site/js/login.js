952c574f8d3ba4c0575d16bee3cd3df419513bfa
Ana Lydia Moreira <analydiamoreira965@gmail.com>
2022-11-03 21:17:01 -0300

    [AnaLydiaMoreira] Finalizacao Pagina-Noticia, notica.css e  implementacao JS

---
+++ br/devs/site/js/login.js
@@ -0,0 +1,21 @@
+function logar() {
+  var login = document.getElementById("username").value;
+  var senha = document.getElementById("password").value;
+  if (login == "admin" && senha == "admin") {
+    document.getElementById("logado").style.display = "block";
+    document.getElementById("logado-erro").style.display = "none";
+  } else {
+    document.getElementById("logado-erro").style.display = "block";
+    document.getElementById("logado").style.display = "none";
+  }
+}
+function loginForm() {
+  var formLogin = document.getElementById("login-form");
+  if (formLogin.style.display == "block") {
+    formLogin.style.display = "none";
+    document.getElementById("logado-erro").style.display = "none";
+    document.getElementById("logado").style.display = "none";
+  } else {
+    document.getElementById("login-form").style.display = "block";
+  }
+}
